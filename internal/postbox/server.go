526d6ff8031d3c9e7370d6d1a27ec1a3a9b19ab4
yongj.zhuang <yongj.zhuang@outlook.com>
2024-06-01 17:52:33 +0800

    Enable logbot middleware

--- internal/postbox/server.go
+++ internal/postbox/server.go
@@ -1,12 +1,14 @@
 package postbox
 
 import (
+	"github.com/curtisnewbie/miso/middleware/logbot"
 	"github.com/curtisnewbie/miso/middleware/user-vault/common"
 	"github.com/curtisnewbie/miso/miso"
 )
 
 func BootstrapServer(args []string) {
 	common.LoadBuiltinPropagationKeys()
+	logbot.EnableLogbotErrLogReport()
 	miso.PreServerBootstrap(RegisterRoutes)
 	miso.BootstrapServer(args)
 }
